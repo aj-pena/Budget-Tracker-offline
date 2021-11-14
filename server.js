574e5cd80217a1b4ba01e8fb2a2cb949b9f5940f
Andres Pena <penchjpc@gmail.com>
2021-11-14 14:28:36 -0500

    reverted heroku app link

--- server.js
+++ server.js
@@ -15,10 +15,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/BudgetTracker", {
+mongoose.connect("mongodb://localhost/budget", 
+{
   useNewUrlParser: true,
-  userUnifiedTopology: true,
-  useCreateIndex: true,
   useFindAndModify: false
 });
 
